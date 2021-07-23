678c166d5c5e59ef0148e0aa72a78518b2d2575e
강규현 <kang@gmail.com>
2021-07-23 10:53:26 +0900

    ALLOWD_HOSTS 변경3\

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,17 +1,17 @@
 from .base import *
 
 ALLOWED_HOSTS = ['3.36.100.105']
-STATIC_ROOT = BASE_DIR / 'static/'
-STATICFILES_DIRS = []
-DEBUG = False
+# STATIC_ROOT = BASE_DIR / 'static/'
+# STATICFILES_DIRS = []
+# DEBUG = False
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'pybo',
-        'USER': 'dbmasteruser',
-        'PASSWORD': '=eKmx$xxymnxxwNCxxx$SX55*RdjKK1G&',
-        'HOST': 'ls-be78fd2cxxxxx614420dxxxxx6b156e2c9.cqlcyugj7ibs.ap-northeast-2.rds.amazonaws.com',
-        'PORT': '5432',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'pybo',
+#         'USER': 'dbmasteruser',
+#         'PASSWORD': '=eKmx$xxymnxxwNCxxx$SX55*RdjKK1G&',
+#         'HOST': 'ls-be78fd2cxxxxx614420dxxxxx6b156e2c9.cqlcyugj7ibs.ap-northeast-2.rds.amazonaws.com',
+#         'PORT': '5432',
+#     }
+# }
