a55fc68c42fc83038992fb0f47f00387f3a7acb4
강규현 <kang@gmail.com>
2021-07-23 17:45:59 +0900

    log기능

--- config/settings/base.py
+++ config/settings/base.py
@@ -185,9 +185,9 @@ LOGGING = {
             'level': 'INFO',
             'propagate': False,
         },
-        # 'pybo': {
-        #     'handlers': ['console', 'file'],
-        #     'level': 'INFO',
-        # },
+        'pybo': {
+            'handlers': ['console', 'file'],
+            'level': 'INFO',
+        },
     }
 }
