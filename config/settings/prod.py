222be3b3c71bcf2590907d0b978d2c8c2bc26fc3
강규현 <kang@gmail.com>
2021-07-24 09:50:52 +0900

    log기능

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -5,13 +5,13 @@ STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
 DEBUG = False
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'pybo',
-        'USER': 'dbmasteruser',
-        'PASSWORD': 'ft$Z.&*zV4]TF<+w9K8iKdfE,L&0rwXR',
-        'HOST': 'ls-172d26327cc36afd21938854b35dc43bfca864c6.cx66xvuli0zc.ap-northeast-2.rds.amazonaws.com',
-        'PORT': '5432',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'pybo',
+#         'USER': 'dbmasteruser',
+#         'PASSWORD': 'ft$Z.&*zV4]TF<+w9K8iKdfE,L&0rwXR',
+#         'HOST': 'ls-172d26327cc36afd21938854b35dc43bfca864c6.cx66xvuli0zc.ap-northeast-2.rds.amazonaws.com',
+#         'PORT': '5432',
+#     }
+# }
